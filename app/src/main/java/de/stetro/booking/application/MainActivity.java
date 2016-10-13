e980265aa0e50201757012a6f1b449c50456cb1d
Steffen Tröster <stroester@inovex.de>
2016-10-13 21:20:45 +0200

    adding basic libraries

--- app/src/main/java/de/stetro/booking/application/MainActivity.java
+++ app/src/main/java/de/stetro/booking/application/MainActivity.java
@@ -1,7 +1,12 @@
 package de.stetro.booking.application;
 
+import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> strings = new ArrayList<>();
+        strings = strings.stream()
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 }
