60e536f4c5227c6180ae595bae178d41386b9031
Steffen Tröster <stroester@inovex.de>
2016-10-14 17:29:10 +0200

    adding hotel structure

--- app/src/main/java/de/stetro/booking/application/data/Hotel.java
+++ app/src/main/java/de/stetro/booking/application/data/Hotel.java
@@ -2,6 +2,18 @@ package de.stetro.booking.application.data;
 
 public class Hotel {
     private String name;
+    private String imageUrl;
+    private String description;
+    private String rating;
+    private Integer stars;
+
+    public Hotel(String name, String imageUrl, String description, String rating, Integer stars) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.rating = rating;
+        this.stars = stars;
+    }
 
     public Hotel(String name) {
         this.name = name;
@@ -13,5 +25,37 @@ public class Hotel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Integer getStars() {
+        return stars;
+    }
+
+    public void setStars(Integer stars) {
+        this.stars = stars;
     }
 }
